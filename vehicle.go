667db81dd5658595f910ad744ddcce6f30a08e67
Lbiza <lbizagwira@gmail.com>
2020-12-07 16:50:04 +0000

    done 1 st task

--- vehicle.go
+++ vehicle.go
@@ -1,8 +1,40 @@
 package main
 
 // Values array for the feedback.json file
-
+type Values struct{
+	Models []Model `json:"values"`
+}
 // Model array for the feedback.json file
+type Model struct{
+	Name string `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+type feedbackResult struct{
+	feedbackTotal int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral int
+}
+type vehicle interface{
+
+}
+type car struct{
+	model string
+	make string
+	typeVehicle string
+}
+type truck struct{
+	model string
+	make string
+	typeVehicle string
+}
+type bike struck {
+	model string
+	make string
+}
+
+var vehicleResult map[string]feedbackResult
+var inventory []vehicle
 
 type rating float32
 
@@ -13,10 +45,10 @@ const (
 	initial       rating = 5.0
 	extraNegative rating = -1.2
 )
-
+ 
 func init() {
 
-	/*
+	
 		inventory = []vehicle{
 			bike{"FTR 1200", "Indian"},
 			bike{"Iron 1200", "Harley"},
@@ -30,8 +62,9 @@ func init() {
 			truck{"F-150", "Ford", "Truck"},
 			truck{"RAM1500", "Dodge", "Truck"}}
 
-	*/
-	//	vehicleResult = make(map[string]feedbackResult)
+	
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
